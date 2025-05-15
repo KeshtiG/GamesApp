ae03d44f941f48da05623f44bf36e4fbed130b64
Keshti Gyllinger <gyllinger@gmail.com>
2025-05-15 16:21:08 +0200

    Added a band model

---
+++ GamesApp.Web/Models/Game.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GamesApp.Web.Models;
+
+public class Game
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "You need to enter a game name.")]
+    public string? Name { get; set; }
+
+
+    [Required(ErrorMessage = "You need to enter a game description.")]
+    public string? Description { get; set; }
+
+
+    [Required(ErrorMessage = "You need to enter a game genre.")]
+    public string? Genre { get; set; }
+
+    [Display(Name = "Release year")]
+    [Required(ErrorMessage = "You need to enter a release year.")]
+    public int Year { get; set; }
+}
