8e9b5efb51ca6b8f245687f424c902cf68ccf7bb
Keshti Gyllinger <gyllinger@gmail.com>
2025-05-15 19:20:47 +0200

    Added views for Index and Add Game. Added a Games Controller. Added Bootstrap links and basic custom CSS.

---
+++ GamesApp.Web/Controllers/GamesController.cs
@@ -0,0 +1,32 @@
+﻿using GamesApp.Web.Models;
+using GamesApp.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GamesApp.Web.Controllers
+{
+    public class GamesController : Controller
+    {
+        static GameService gameService = new GameService();
+
+        [HttpGet("")]
+        public IActionResult Index() => View(gameService.GetAllGames());
+
+
+        [HttpGet("/AddGame")]
+        public IActionResult AddGame() => View();
+
+
+        [HttpPost("/AddGame")]
+        public IActionResult AddGame(Game game)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            gameService.AddGame(game);
+            return RedirectToAction(nameof(Index));
+        }
+
+    }
+}
